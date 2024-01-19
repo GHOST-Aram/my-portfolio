aefce03f45607f836de38ddefa9d2a3dbe3cfef5
ghost-aramic <owinoojala@gmail.com>
2024-01-19 16:28:12 +0300

    feat: Display Projects previews

--- src/containers/Projects.tsx
+++ src/containers/Projects.tsx
@@ -1,15 +1,53 @@
-import React from 'react'
 import SectionTitle from '../components/SectionTitle'
+import ImgSrc from '../images/online-liquor-store.png'
+import ProjectPreview from '../components/ProjectPreview'
 
 const Projects = () => {
   return (
     <section>
         <SectionTitle text='Projects'/>
         <div className="container projects">
-
+			{
+				projects.map(project => (
+					<ProjectPreview 
+						title={project.title}
+						imageSrc={project.imageSrc}
+						description={project.description}
+						altText={project.altText}
+					/>
+				))
+			}
         </div>
     </section>
   )
 }
 
+const projects = [
+    {
+		title: 'Online Liqour Store',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '+
+			'Nulla praesentium saepe ullam necessitatibus quidem magnam. '+
+			'Facere expedita, modi quas doloribus amet magnam deserunt consectetur'+
+			' dolore et, enim voluptatibus, aut odio.',
+		altText: 'online liqour store' ,
+		imageSrc: ImgSrc,
+    }, {
+		title: 'Online Liqour Store',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '+
+			'Nulla praesentium saepe ullam necessitatibus quidem magnam. '+
+			'Facere expedita, modi quas doloribus amet magnam deserunt consectetur'+
+			' dolore et, enim voluptatibus, aut odio.',
+		altText: 'online liqour store' ,
+		imageSrc: ImgSrc
+	},
+	{
+		title: 'Online Liqour Store',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '+
+			'Nulla praesentium saepe ullam necessitatibus quidem magnam. '+
+			'Facere expedita, modi quas doloribus amet magnam deserunt consectetur'+
+			' dolore et, enim voluptatibus, aut odio.',
+		altText: 'online liqour store' ,
+		imageSrc: ImgSrc
+	}
+]
 export default Projects
