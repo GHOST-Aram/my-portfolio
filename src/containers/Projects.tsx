7513ef8e1d510e213deacf17c6a8b0a8de5d2c8b
ghost-aramic <owinoojala@gmail.com>
2024-01-21 12:02:27 +0300

    refactor: Create a file for aggregating project details

--- src/containers/Projects.tsx
+++ src/containers/Projects.tsx
@@ -1,7 +1,6 @@
 import SectionTitle from '../components/SectionTitle'
-import liqourImgSrc from '../images/online-liqour-store.png'
 import ProjectPreview from '../components/ProjectPreview'
-import memoryGameImgSrc from '../images/memeory-game.png'
+import { projects } from '../projects-details'
 
 const Projects = () => {
   return (
@@ -22,33 +21,4 @@ const Projects = () => {
     </section>
   )
 }
-
-const projects = [
-    {
-		title: 'Online Liqour Store',
-		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '+
-			'Nulla praesentium saepe ullam necessitatibus quidem magnam. '+
-			'Facere expedita, modi quas doloribus amet magnam deserunt consectetur'+
-			' dolore et, enim voluptatibus, aut odio.',
-		altText: 'online liqour store' ,
-		imageSrc: liqourImgSrc,
-    }, {
-		title: 'Memory Game',
-		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '+
-			'Nulla praesentium saepe ullam necessitatibus quidem magnam. '+
-			'Facere expedita, modi quas doloribus amet magnam deserunt consectetur'+
-			' dolore et, enim voluptatibus, aut odio.',
-		altText: 'online liqour store' ,
-		imageSrc: memoryGameImgSrc
-	},
-	{
-		title: 'Memory Game',
-		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '+
-			'Nulla praesentium saepe ullam necessitatibus quidem magnam. '+
-			'Facere expedita, modi quas doloribus amet magnam deserunt consectetur'+
-			' dolore et, enim voluptatibus, aut odio.',
-		altText: 'online liqour store' ,
-		imageSrc: memoryGameImgSrc
-	}
-]
 export default Projects
---
+++ src/projects-details.ts
@@ -0,0 +1,31 @@
+import liqourImgSrc from './images/online-liqour-store.png'
+import memoryGameImgSrc from './images/memeory-game.png'
+
+export const projects = [
+    {
+		title: 'Online Liqour Store',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '+
+			'Nulla praesentium saepe ullam necessitatibus quidem magnam. '+
+			'Facere expedita, modi quas doloribus amet magnam deserunt consectetur'+
+			' dolore et, enim voluptatibus, aut odio.',
+		altText: 'online liqour store' ,
+		imageSrc: liqourImgSrc,
+    }, {
+		title: 'Memory Game',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '+
+			'Nulla praesentium saepe ullam necessitatibus quidem magnam. '+
+			'Facere expedita, modi quas doloribus amet magnam deserunt consectetur'+
+			' dolore et, enim voluptatibus, aut odio.',
+		altText: 'online liqour store' ,
+		imageSrc: memoryGameImgSrc
+	},
+	{
+		title: 'Memory Game',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. '+
+			'Nulla praesentium saepe ullam necessitatibus quidem magnam. '+
+			'Facere expedita, modi quas doloribus amet magnam deserunt consectetur'+
+			' dolore et, enim voluptatibus, aut odio.',
+		altText: 'online liqour store' ,
+		imageSrc: memoryGameImgSrc
+	}
+]
